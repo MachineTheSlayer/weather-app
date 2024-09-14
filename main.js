5854198a92c4601225ab248c986a056051f2e5b3
MachineTheSlayer <dangodggw@gmail.com>
2024-09-15 02:27:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const apiKey = '27845ce6f357493faf8122557241309';
+// http://api.weatherapi.com/v1/current.json?key=27845ce6f357493faf8122557241309&q=London
+//const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`;
+
+//fetch(query).then ( (response) => {
+ //   return response.json()
+//}).then ( (data) => {
+ //   console.log(data);
+//})
+
+// Элементы на странице
+const header = document.querySelector('.header');
+const form = document.querySelector('#form');
+const input = document.querySelector('#inputCity');
+let city;
+
+function removeCard() {
+    // Удаляем предыдущие карточки
+    const prevCard = document.querySelector('.card');
+    if (prevCard) prevCard.remove();
+}
+
+function showError(errorMessage) {
+	// Отобразить карточку с ошибкой
+	const html = `<div class="card">${errorMessage}</div>`;
+
+	// Отображаем карточку на странице
+	header.insertAdjacentHTML('afterend', html);
+}
+
+function showCard({ name, country, temp, condition }) {
+	// Разметка для карточки
+	const html = `<div class="card">
+                                <h2 class="card-city">${name} <span>${country}</span></h2>
+
+                                <div class="card-weather">
+                                    <div class="card-value">${temp}<sup>°c</sup></div>
+                                    <img class="card-img" src="./img/example.png" alt="Weather">
+                                </div>
+
+                                <div class="card-description">${condition}</div>
+                            </div>`;
+
+	// Отображаем карточку на странице
+	header.insertAdjacentHTML('afterend', html);
+}
+
+async function getWeather(city) {
+	const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+    return data;
+}
+
+// Слушаем отправку формы
+form.onsubmit = async function (e) {
+	// Отменяем отправку формы
+	e.preventDefault();
+
+	// Берем значение из инпута, обрезаем пробелы
+	let city = input.value.trim();
+
+    // Получаем данные с сервера
+    const data = await getWeather(city);
+
+    if (data.error) {
+		removeCard();
+		showError(data.error.message);
+	} else {
+		removeCard();
+
+        const weatherData = {
+			name: data.location.name,
+			country: data.location.country,
+			temp: data.current.temp_c,
+			condition: data.current.condition.text,
+		};
+
+		showCard(weatherData);
+	}
+
+};
